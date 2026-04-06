4766585aeaca345cc2419de17d5f79624322127c
VINNSTOREE <vingg1633@gmail.com>
2026-04-07 01:03:17 +0700

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,20 +1,9 @@
-import fetch from "node-fetch"
 import config from "./config.js"
-import { get, update, remove } from "./db.js"
 
 export default async function handler(req, res){
   const { id } = req.query
 
   if(!id) return res.json({ error: "ID kosong" })
-
-  const trx = get(id)
-  if(!trx) return res.json({ error: "Transaksi tidak ditemukan" })
-
-  // ⏳ CEK EXPIRED
-  if(Date.now() - trx.createdAt > config.EXPIRED_TIME){
-    remove(id)
-    return res.json({ status: "expired" })
-  }
 
   try{
     const cek = await fetch(`${config.BASE_URL}/deposit/status/${id}`, {
@@ -24,9 +13,12 @@ export default async function handler(req, res){
     })
 
     const data = await cek.json()
-    const status = data?.data?.status || "unknown"
 
-    update(id, { status })
+    const status = data?.data?.status
+
+    if(!status){
+      return res.json({ status: "unknown" })
+    }
 
     return res.json({
       success: true,
